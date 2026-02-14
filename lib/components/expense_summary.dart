f89db8f390fe68738d74f16e456a310a4d64d38f
ajinkya ghode <ajinkyaghode85@gmail.com>
2026-02-14 14:51:43 +0530

    todays changes and updates

--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/DateTime/date_time_helper.dart';
 import 'package:expense_tracker/bar%20graph/bar_graph.dart';
 import 'package:expense_tracker/data/expense_data.dart';
 import 'package:flutter/material.dart';
@@ -10,18 +11,40 @@ class ExpenseSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String sunday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 0)),
+    );
+    String monday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 1)),
+    );
+    String tuesday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 2)),
+    );
+    String wednesday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 3)),
+    );
+    String thursday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 4)),
+    );
+    String friday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 5)),
+    );
+    String saturday = converDateTimeToString(
+      startOfWeek.add(const Duration(days: 6)),
+    );
+
     return Consumer<ExpenseData>(
       builder: (context, value, child) => SizedBox(
         height: 200,
         child: MyBarGraph(
-          maxY: 200,
-          monAmount: 12,
-          tueAmount: 30,
-          wedAmount: 50,
-          thurAmount: 34,
-          friAmount: 54,
-          satAmount: 23,
-          sunAmount: 54,
+          maxY: 100,
+          monAmount: value.calculateDailyExpenseSummery()[sunday] ?? 0,
+          tueAmount: value.calculateDailyExpenseSummery()[monday] ?? 0,
+          wedAmount: value.calculateDailyExpenseSummery()[tuesday] ?? 0,
+          thurAmount: value.calculateDailyExpenseSummery()[wednesday] ?? 0,
+          friAmount: value.calculateDailyExpenseSummery()[thursday] ?? 0,
+          satAmount: value.calculateDailyExpenseSummery()[friday] ?? 0,
+          sunAmount: value.calculateDailyExpenseSummery()[saturday] ?? 0,
         ),
       ),
     );
